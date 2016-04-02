f6b438a54eae16467e14cfe8ebf0b157dcf12e04
jan p springer <jsd@igroup.org>
2016-04-02 09:48:29 +0100

    added: prototype for main program

---
+++ share/hugh/proto/proto.main.cpp
@@ -0,0 +1,44 @@
+// -*- Mode:C++ -*-
+
+/**************************************************************************************************/
+/*                                                                                                */
+/* Copyright (C) 2016 University of Hull                                                          */
+/*                                                                                                */
+/**************************************************************************************************/
+/*                                                                                                */
+/*  module     :  hugh/proto.cpp                                                                  */
+/*  project    :                                                                                  */
+/*  description:                                                                                  */
+/*                                                                                                */
+/**************************************************************************************************/
+
+// includes, system
+
+#include <cstdlib> // EXIT_SUCCESS
+
+// includes, project
+
+#include <>
+
+#define HUGH_USE_TRACE
+#undef HUGH_USE_TRACE
+#include <hugh/support/trace.hpp>
+
+// internal unnamed namespace
+
+namespace {
+  
+  // types, internal (class, enum, struct, union, typedef)
+
+  // variables, internal
+  
+  // functions, internal
+
+} // namespace {
+
+int
+main(int /* argc */, char const* /* argv */[])
+{
+  return EXIT_SUCCESS;
+}
+
