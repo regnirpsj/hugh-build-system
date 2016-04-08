eb277bd9d3db429b0a8951622f73fb68dd6bae86
jan p springer <jsd@igroup.org>
2016-04-08 15:43:51 +0100

    update: default hunter setup

--- share/hugh/cmake/SetupHunter.cmake
+++ share/hugh/cmake/SetupHunter.cmake
@@ -6,6 +6,18 @@
 #                                                                                                  #
 ####################################################################################################
 
-include(HunterGate)
+include(${CMAKE_CURRENT_LIST_DIR}/HunterGate.cmake)
+
+option(HUNTER_ENABLED "Enable Hunter package manager" OFF)
+
+if(VERBOSE)
+  set(HUNTER_STATUS_PRINT ON)
+endif()
+
+if(DEBUG)
+  set(HUNTER_STATUS_DEBUG)
+endif()
+
 HunterGate(URL  "https://github.com/ruslo/hunter/archive/v0.14.1.tar.gz"
            SHA1 "d073cc04d59a01782385799568afce78066a8cec")
+
