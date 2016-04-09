8ec07bf26ca8dcf9ec92400bf8134c877e8f8b46
jan p springer <jsd@igroup.org>
2016-04-09 22:16:16 +0100

    fixed: HUGH_INSTALL_PREFIX handling

--- share/hugh/cmake/SetupInstallPrefix.cmake
+++ share/hugh/cmake/SetupInstallPrefix.cmake
@@ -7,13 +7,15 @@
 ####################################################################################################
 
 if(DEFINED ENV{HUGH_INSTALL_PREFIX})
-  set(CMAKE_INSTALL_PREFIX $ENV{HUGH_INSTALL_PREFIX})
-else()
-  if(HUGH_INSTALL_PREFIX)
-    set(CMAKE_INSTALL_PREFIX ${HUGH_INSTALL_PREFIX})
-  endif()
+  set(HUGH_INSTALL_PREFIX $ENV{HUGH_INSTALL_PREFIX} CACHE INTERNAL "HUGH Installation Root")
 endif()
 
-if(VERBOSE)
+if(NOT HUGH_INSTALL_PREFIX)
+  set(HUGH_INSTALL_PREFIX ${CMAKE_SOURCE_DIR}/install CACHE INTERNAL "HUGH Installation Root")
+endif()
+
+set(CMAKE_INSTALL_PREFIX ${HUGH_INSTALL_PREFIX})
+
+if(TRUE OR VERBOSE)
   message(STATUS "CMAKE_INSTALL_PREFIX set to '" ${CMAKE_INSTALL_PREFIX} "'")
 endif()
