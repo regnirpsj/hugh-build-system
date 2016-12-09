bee65be02f5777087ad9746346d985db640a7267
jan p springer <jsd@igroup.org>
2016-12-09 18:55:19 +0000

    update: MSVC compile flags (warning level + language extensions)

--- share/hugh/cmake/compiler/msvc.cmake
+++ share/hugh/cmake/compiler/msvc.cmake
@@ -22,7 +22,7 @@ set(DISABLED_WARNINGS)
 list(APPEND DISABLED_WARNINGS /wd4100)
 
 # nonstandard extension used : nameless struct/union
-list(APPEND DISABLED_WARNINGS /wd4201)
+#list(APPEND DISABLED_WARNINGS /wd4201)
                                                       
 if("${CMAKE_LIBRARY_TYPE}" STREQUAL "SHARED")
   # 'identifier' : class 'type' needs to have dll-interface to be used by clients of class 'type2'
@@ -56,7 +56,10 @@ list(APPEND DISABLED_WARNINGS /wd4702)
 # C1128: number of sections exceeded object file format limit: compile with /bigobj
 list(APPEND DISABLED_WARNINGS /bigobj)
 
+# warning level + language extensions:
+# [https://github.com/g-truc/glm/pull/587#issuecomment-265731442]
 list(APPEND GLOBAL_COMPILER_FLAGS /W4)  # warn almost everything
+list(APPEND GLOBAL_COMPILER_FLAGS /Za)  # disable language extension
 list(APPEND GLOBAL_COMPILER_FLAGS ${DISABLED_WARNINGS})
 list(APPEND GLOBAL_COMPILER_FLAGS /EHa) # exception-handling for asynchronous (structured) and
                                         # synchronous (C++) exceptions
